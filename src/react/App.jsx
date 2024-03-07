2e41b5f9cfc86c9bc739bf8610f4585a0dd75c7c
Tolgahan Arikan <tolgahan.arikan@gmail.com>
2024-03-07 21:06:47 +0300

    fix sendTransaction usage, use WagmiProvider instead of WagmiConfig

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { useOpenConnectModal } from '@0xsequence/kit'
-import { useDisconnect, useAccount } from 'wagmi'
-import MyPage from './MyPage.jsx'
-import SignOut from './SignOut.jsx'
+import React from "react";
+import { useOpenConnectModal } from "@0xsequence/kit";
+import { useDisconnect, useAccount } from "wagmi";
+import MyPage from "./MyPage.jsx";
+import SignOut from "./SignOut.jsx";
 
-import { KitProvider, getKitConnectWallets } from '@0xsequence/kit'
-import { getDefaultConnectors, getDefaultWaasConnectors, mock } from '@0xsequence/kit-connectors'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { createConfig, http, WagmiConfig } from 'wagmi'
-import { mainnet, polygon, Chain } from 'wagmi/chains'
+import { KitProvider, getKitConnectWallets } from "@0xsequence/kit";
+import {
+  getDefaultConnectors,
+  getDefaultWaasConnectors,
+  mock,
+} from "@0xsequence/kit-connectors";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createConfig, http, WagmiProvider } from "wagmi";
+import { mainnet, polygon, Chain } from "wagmi/chains";
 
-const queryClient = new QueryClient() 
+const queryClient = new QueryClient();
 
-const chains = [mainnet, polygon]
+const chains = [mainnet, polygon];
 //   const projectAccessKey = ENV.projectAccessKey
 
 //   const connectors = getDefaultConnectors({
@@ -22,59 +26,54 @@ const chains = [mainnet, polygon]
 //     projectAccessKey
 //   })
 
-  const projectAccessKey = 'EeP6AmufRFfigcWaNverI6CAAAAAAAAAA'
-  const waasConfigKey =
-    'eyJwcm9qZWN0SWQiOjIsImVtYWlsUmVnaW9uIjoidXMtZWFzdC0yIiwiZW1haWxDbGllbnRJZCI6IjVncDltaDJmYnFiajhsNnByamdvNzVwMGY2IiwicnBjU2VydmVyIjoiaHR0cHM6Ly9uZXh0LXdhYXMuc2VxdWVuY2UuYXBwIn0='
-  const googleClientId = '970987756660-35a6tc48hvi8cev9cnknp0iugv9poa23.apps.googleusercontent.com'
-  const appleClientId = 'com.horizon.sequence.waas'
-  const appleRedirectURI = 'https://strong-pavlova-dcf6f0.netlify.app'
+const projectAccessKey = "EeP6AmufRFfigcWaNverI6CAAAAAAAAAA";
+const waasConfigKey =
+  "eyJwcm9qZWN0SWQiOjIsImVtYWlsUmVnaW9uIjoidXMtZWFzdC0yIiwiZW1haWxDbGllbnRJZCI6IjVncDltaDJmYnFiajhsNnByamdvNzVwMGY2IiwicnBjU2VydmVyIjoiaHR0cHM6Ly9uZXh0LXdhYXMuc2VxdWVuY2UuYXBwIn0=";
+const googleClientId =
+  "970987756660-35a6tc48hvi8cev9cnknp0iugv9poa23.apps.googleusercontent.com";
+const appleClientId = "com.horizon.sequence.waas";
+const appleRedirectURI = "https://strong-pavlova-dcf6f0.netlify.app";
 
-  const connectors = [
-    ...getDefaultWaasConnectors({
-      walletConnectProjectId: 'c65a6cb1aa83c4e24500130f23a437d8',
-      defaultChainId: 137,
-      waasConfigKey,
-      googleClientId,
-      appleClientId,
-      appleRedirectURI,
-      appName: 'demo app',
-      projectAccessKey,
-      enableConfirmationModal: true
-    }),
-    ...(getKitConnectWallets(projectAccessKey, [
-          mock({
-            accounts: ['0xCb88b6315507e9d8c35D81AFB7F190aB6c3227C9']
-          })
-        ]))
-  ]
+const connectors = [
+  ...getDefaultWaasConnectors({
+    walletConnectProjectId: "c65a6cb1aa83c4e24500130f23a437d8",
+    defaultChainId: 137,
+    waasConfigKey,
+    googleClientId,
+    appleClientId,
+    appleRedirectURI,
+    appName: "demo app",
+    projectAccessKey,
+    enableConfirmationModal: true,
+  }),
+  ...getKitConnectWallets(projectAccessKey, []),
+];
 
-  const transports = {}
+const transports = {};
 
-  chains.forEach(chain => {
-    transports[chain.id] = http()
-  })
-  
-  const config = createConfig({
-    transports,
-    connectors,
-    chains
-  })
-  
+chains.forEach((chain) => {
+  transports[chain.id] = http();
+});
+
+const config = createConfig({
+  transports,
+  connectors,
+  chains,
+});
+
 function App(props) {
-
-
   return (
-    <WagmiConfig config={config}>
-        <QueryClientProvider client={queryClient}> 
-        <KitProvider config={{defaultTheme: 'light'}}>
-          <MyPage scene={props.scene}/>
-          <SignOut scene={props.scene}/>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <KitProvider config={{ defaultTheme: "light" }}>
+          <MyPage scene={props.scene} />
+          <SignOut scene={props.scene} />
         </KitProvider>
-        </QueryClientProvider>
-    </WagmiConfig>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
 
 export default App;
 
-export {config};
+export { config };
--- src/react/MyPage.jsx
+++ src/react/MyPage.jsx
@@ -1,112 +1,144 @@
-import React, {useEffect} from 'react'
-import { useOpenConnectModal } from '@0xsequence/kit'
-import { useDisconnect, useAccount, useWalletClient, useSendTransaction } from 'wagmi'
-import './styles.css'
+import React, { useEffect } from "react";
+import { useOpenConnectModal } from "@0xsequence/kit";
+import {
+  useDisconnect,
+  useAccount,
+  useWalletClient,
+  useSendTransaction,
+} from "wagmi";
+import "./styles.css";
 
-import {ethers} from 'ethers'
-import {config} from './App.jsx'
+import { ethers } from "ethers";
+import { config } from "./App.jsx";
 const AuthModes = {
-    Email: "email",
-    Code: "code",
-    Completed: "completed"
-}
+  Email: "email",
+  Code: "code",
+  Completed: "completed",
+};
 
 const GameModes = {
-	Intro: "intro",
-	Playing: "playing",
-	Paused: "paused",
-	GameEnding: "gameending",
-	GameOver: "gameover",
+  Intro: "intro",
+  Playing: "playing",
+  Paused: "paused",
+  GameEnding: "gameending",
+  GameOver: "gameover",
   CardWon: "cardwon",
   CardReady: "cardready",
   SigningOut: "signingout",
   CardDetails: "carddetails",
-}
+};
 
 const ContractAddress = "0xbb35dcf99a74b4a6c38d69789232fa63e1e69e31";
 
 function MyPage(props) {
-    const { setOpenConnectModal } = useOpenConnectModal()
+  const { setOpenConnectModal } = useOpenConnectModal();
 
-    const { isConnected } = useAccount()
-    const {disconnect} = useDisconnect()
-    const { data: walletClient } = useWalletClient()
-    const { data: txnData, sendTransaction, isLoading: isSendTxnLoading } = useSendTransaction()
-    const onClick = () => {
-        setOpenConnectModal(true)
+  const { isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
+  const { data: walletClient } = useWalletClient();
+  const {
+    data: txnData,
+    sendTransaction,
+    isLoading: isSendTxnLoading,
+  } = useSendTransaction();
+  const onClick = () => {
+    setOpenConnectModal(true);
+  };
+
+  window.setOpenConnectModal = () => {
+    setOpenConnectModal(true);
+    setTimeout(
+      () =>
+        (document.getElementById("email").style.backgroundColor =
+          "transparent"),
+      0
+    );
+    setTimeout(
+      () => (document.getElementById("email").style.border = "0px"),
+      0
+    );
+    setTimeout(
+      () => (document.getElementById("email").style.margin = "10px"),
+      0
+    );
+    setTimeout(
+      () => (document.getElementById("email").style.marginLeft = "-10px"),
+      0
+    );
+  };
+
+  useEffect(() => {
+    setInterval(() => {
+      if (document.getElementById("webpack-dev-server-client-overlay"))
+        document.getElementById("webpack-dev-server-client-overlay").remove();
+    }, 100);
+
+    if (isConnected && walletClient) {
+      console.log(walletClient);
+      props.scene.sequenceController.init(walletClient, sendBurnToken);
+      props.scene.switchGameMode(GameModes.Intro);
+      props.scene.sequenceController.switchAuthMode(AuthModes.Completed);
     }
+  }, [isConnected, walletClient]);
 
-    window.setOpenConnectModal = () => {
-        setOpenConnectModal(true)
-        setTimeout(() => document.getElementById('email').style.backgroundColor = 'transparent', 0)
-        setTimeout(() => document.getElementById('email').style.border = '0px', 0)
-        setTimeout(() => document.getElementById('email').style.margin = '10px', 0)
-        setTimeout(() => document.getElementById('email').style.marginLeft = '-10px', 0)
+  const sendBurnToken = async (tokenID, amount, callback) => {
+    const contractABI = ["function burn(uint256 tokenId, uint256 amount)"]; // Replace with your contract's ABI
+    const contract = new ethers.Contract(ContractAddress, contractABI);
+    const data = contract.interface.encodeFunctionData("burn", [
+      tokenID,
+      amount,
+    ]);
+
+    try {
+      console.log(sendTransaction);
+      sendTransaction({
+        to: ContractAddress,
+        data: data,
+        value: "0",
+        gas: null,
+      });
+
+      callback(ResizeObserver);
+    } catch (error) {
+      callback(error);
     }
+  };
 
-    useEffect(() => {
-        setInterval(() => {
-            if(document.getElementById('webpack-dev-server-client-overlay')) document.getElementById('webpack-dev-server-client-overlay').remove()
-        }, 100)
-
-        if(isConnected&& walletClient){
-            console.log(walletClient)
-            props.scene.sequenceController.init(walletClient, sendBurnToken)
-            props.scene.switchGameMode(GameModes.Intro)
-            props.scene.sequenceController.switchAuthMode(AuthModes.Completed)
-        }
-    }, [isConnected, walletClient])
-
-    const sendBurnToken = async (tokenID, amount, callback) => {
-        const contractABI = ['function burn(uint256 tokenId, uint256 amount)']; // Replace with your contract's ABI
-      const contract = new ethers.Contract(ContractAddress, contractABI);
-      const data = contract.interface.encodeFunctionData('burn', [tokenID,amount]);
-  
-      try {
-        console.log(sendTransaction)
-        const res = await sendTransaction(config,{
-          to: ContractAddress,
-          data: data,
-          value: '0',
-          gas: null
-        })
-        console.log(res)
-        console.log(isSendTxnLoading)
-        console.log(txnData)
-        callback(ResizeObserver)
-      }catch(error){
-        callback(error)
-      }
+  useEffect(() => {
+    if (txnData) {
+      console.log(txnData);
     }
+  }, [isSendTxnLoading, txnData]);
 
-    useEffect(() => {
+  return (
+    <>
+      <div style={{ textAlign: "center", zIndex: 100 }}>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
 
-    },[isSendTxnLoading, txnData])
-
-    return (
-        <>
-        <div style={{textAlign:'center', zIndex: 100}}>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            {!isConnected &&<div className='message message--replay' style={{cursor: 'pointer'}}>
-                Click to login
-            </div> }
-            <br/>
-            <br/>
-            <br/>
-        </div>
-        </>
-    )
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        {!isConnected && (
+          <div
+            className="message message--replay"
+            style={{ cursor: "pointer" }}
+          >
+            Click to login
+          </div>
+        )}
+        <br />
+        <br />
+        <br />
+      </div>
+    </>
+  );
 }
 
-export default MyPage
+export default MyPage;
