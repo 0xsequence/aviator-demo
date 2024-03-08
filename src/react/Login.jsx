c605e7384c8614ac2165b00aefbbff5fb368c9d0
Tolgahan Arikan <tolgahan.arikan@gmail.com>
2024-03-08 17:37:58 +0300

    Update to new kit beta version

--- src/react/Login.jsx
+++ src/react/Login.jsx
@@ -1,39 +1,39 @@
-import React, { useEffect, useState } from "react";
-import { useOpenConnectModal } from "@0xsequence/kit";
+import React, { useEffect, useState } from 'react';
+import { useOpenConnectModal } from '@0xsequence/kit';
 import {
   useDisconnect,
   useAccount,
   useWalletClient,
   useSendTransaction,
-} from "wagmi";
-import "./styles.css";
+} from 'wagmi';
+import './styles.css';
 
-import { ethers } from "ethers";
-import { config } from "./App.jsx";
+import { ethers } from 'ethers';
+import { config } from './App.jsx';
 
 const AuthModes = {
-  Email: "email",
-  Code: "code",
-  Completed: "completed",
+  Email: 'email',
+  Code: 'code',
+  Completed: 'completed',
 };
 
 const GameModes = {
-  Intro: "intro",
-  Playing: "playing",
-  Paused: "paused",
-  GameEnding: "gameending",
-  GameOver: "gameover",
-  CardWon: "cardwon",
-  CardReady: "cardready",
-  SigningOut: "signingout",
-  CardDetails: "carddetails",
+  Intro: 'intro',
+  Playing: 'playing',
+  Paused: 'paused',
+  GameEnding: 'gameending',
+  GameOver: 'gameover',
+  CardWon: 'cardwon',
+  CardReady: 'cardready',
+  SigningOut: 'signingout',
+  CardDetails: 'carddetails',
 };
 
-const ContractAddress = "0xbb35dcf99a74b4a6c38d69789232fa63e1e69e31";
+const ContractAddress = '0xbb35dcf99a74b4a6c38d69789232fa63e1e69e31';
 
 function Login(props) {
   const { setOpenConnectModal } = useOpenConnectModal();
-  const [burnCallback, setBurnCallback] = useState(null)
+  const [burnCallback, setBurnCallback] = useState(null);
   const { isConnected } = useAccount();
   const { disconnect } = useDisconnect();
   const { data: walletClient } = useWalletClient();
@@ -50,28 +50,28 @@ function Login(props) {
     setOpenConnectModal(true);
     setTimeout(
       () =>
-        (document.getElementById("email").style.backgroundColor =
-          "transparent"),
+        (document.getElementById('email').style.backgroundColor =
+          'transparent'),
       0
     );
     setTimeout(
-      () => (document.getElementById("email").style.border = "0px"),
+      () => (document.getElementById('email').style.border = '0px'),
       0
     );
     setTimeout(
-      () => (document.getElementById("email").style.margin = "10px"),
+      () => (document.getElementById('email').style.margin = '10px'),
       0
     );
     setTimeout(
-      () => (document.getElementById("email").style.marginLeft = "-10px"),
+      () => (document.getElementById('email').style.marginLeft = '-10px'),
       0
     );
   };
 
   useEffect(() => {
     setInterval(() => {
-      if (document.getElementById("webpack-dev-server-client-overlay"))
-        document.getElementById("webpack-dev-server-client-overlay").remove();
+      if (document.getElementById('webpack-dev-server-client-overlay'))
+        document.getElementById('webpack-dev-server-client-overlay').remove();
     }, 100);
 
     if (isConnected && walletClient) {
@@ -83,19 +83,18 @@ function Login(props) {
   }, [isConnected, walletClient]);
 
   const sendBurnToken = async (tokenID, amount, callback) => {
-    const contractABI = ["function burn(uint256 tokenId, uint256 amount)"]; // Replace with your contract's ABI
+    const contractABI = ['function burn(uint256 tokenId, uint256 amount)']; // Replace with your contract's ABI
     const contract = new ethers.Contract(ContractAddress, contractABI);
-    const data = contract.interface.encodeFunctionData("burn", [
+    const data = contract.interface.encodeFunctionData('burn', [
       tokenID,
       amount,
     ]);
 
     try {
-      console.log(sendTransaction);
       sendTransaction({
         to: ContractAddress,
         data: data,
-        value: "0",
+        value: '0',
         gas: null,
       });
     } catch (error) {
@@ -105,18 +104,15 @@ function Login(props) {
 
   useEffect(() => {
     if (txnData) {
-    setTimeout(() => {
-
-    }, 1000)
       console.log(txnData);
-      props.scene.closeCardModal()
+      props.scene.closeCardModal();
       props.scene.sequenceController.fetchWalletTokens();
     }
   }, [isSendTxnLoading, txnData]);
 
   return (
     <>
-      <div style={{ textAlign: "center", zIndex: 100 }}>
+      <div style={{ textAlign: 'center', zIndex: 100 }}>
         <br />
         <br />
         <br />
@@ -131,7 +127,7 @@ function Login(props) {
         {!isConnected && (
           <div
             className="message message--replay"
-            style={{ cursor: "pointer" }}
+            style={{ cursor: 'pointer' }}
           >
             Click to login
           </div>
