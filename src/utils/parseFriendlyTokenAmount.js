e1a87eb364a93f39f3c2f4160aab681c9d80ef04
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2024-05-24 09:27:34 -0700

    ui/ux improvements to grids in hangar and market

--- src/utils/parseFriendlyTokenAmount.js
+++ src/utils/parseFriendlyTokenAmount.js
@@ -1,8 +1,18 @@
-export function parseFriendlyTokenAmount(v, decimals) {
-  const num = Number(v) / 10 ** 18
-    if (num >= 0.01) {
-        return num.toFixed(decimals);
-      } else {
-        return 0
-      }
+export function parseFriendlyTokenAmount(v) {
+  const num = Number(v) / 10 ** 18;
+  if (num >= 0.01) {
+    if (num < 10) {
+      return num.toFixed(2);
+    } else if(num<100) {
+      return num.toFixed(1);
+    } else if(num<1000) {
+      return Math.round(num).toFixed(0);
+    } else if(num<10000) {
+      return (num/1000).toFixed(1) + 'K';
+    } else {
+      return (num/1000).toFixed(0) + 'K';
+    }
+  } else {
+    return 0;
+  }
 }
